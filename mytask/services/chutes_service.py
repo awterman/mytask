fdf8b66ea24b41046ccc2b260c171cffa0e791bb
awterman <awterman@users.noreply.github.com>
2025-04-16 05:59:15 +0800

    Chutes doesn't support JSON output

--- mytask/services/chutes_service.py
+++ mytask/services/chutes_service.py
@@ -2,12 +2,10 @@ import json
 from typing import List
 
 from openai import AsyncOpenAI
-from openai.types.chat.chat_completion_user_message_param import (
-    ChatCompletionUserMessageParam,
-)
-from openai.types.shared_params.response_format_json_object import (
-    ResponseFormatJSONObject,
-)
+from openai.types.chat.chat_completion_user_message_param import \
+    ChatCompletionUserMessageParam
+from openai.types.shared_params.response_format_json_object import \
+    ResponseFormatJSONObject
 
 
 class ChutesService:
@@ -18,10 +16,6 @@ class ChutesService:
 
     def __init__(self, api_key: str):
         base_url = "https://llm.chutes.ai/v1"
-
-        # Local testing because Chutes is unreachable
-        api_key = "sk-71MDVSXD1Hkjguu9kqWP8g"
-        base_url = "http://192.168.50.213:4000"
 
         self.llm = AsyncOpenAI(api_key=api_key, base_url=base_url)
 
@@ -38,7 +32,7 @@ class ChutesService:
         Returns:
             int: The sentiment score for the tweets
         """
-        model = "openrouter/google/gemini-2.0-flash-001"
+        # model = "openrouter/google/gemini-2.0-flash-001"
         # Create a context with tweets for the prompt
         tweet_context = "\n\n".join([f"Tweet: {tweet}" for tweet in tweets])
 
@@ -53,9 +47,7 @@ class ChutesService:
         {tweet_context}
 
         Output in JSON format:
-        {{
-            "score": int,
-        }}
+        [-1, 0, 1]
         
         """
 
@@ -68,11 +60,33 @@ class ChutesService:
                 ),
             ],
             temperature=0.0,
-            response_format=ResponseFormatJSONObject(
-                type="json_object",
-            ),
         )
 
         output = response.choices[0].message.content
         assert output is not None
-        return json.loads(output)["score"]
+
+        # Parse the output, find the block starting with '[' and ending with ']'
+        start = output.find("[")
+        end = output.rfind("]") + 1
+        json_output = output[start:end]
+        scores = json.loads(json_output)
+        return int(sum(scores) / len(scores))
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    from mytask.common.settings import get_settings
+    settings = get_settings()
+
+    service = ChutesService(
+        settings.chutes_api_key
+    )
+    async def main():
+        print(
+            await service.score_tweet_sentiment(
+                ["I love this product!", "I hate this product!"]
+            )
+        )
+
+    asyncio.run(main())
